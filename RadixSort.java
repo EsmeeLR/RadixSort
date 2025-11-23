f74d313ab3afa06d28c488dfbf9e6fb6bef192f4
EsmeeLR <esmeralda.lunaromano14mail.com>
2025-11-23 16:15:41 -0600

    Metodo principal

--- RadixSort.java
+++ RadixSort.java
@@ -1,3 +1,24 @@
-public class RadixSort{
-    
+public class RadixSort {
+
+    // Método principal
+    public static void radixSort(int[] arr) {
+        int max = obtenerMaximo(arr);
+
+        // Aplicar counting sort para cada dígito
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            countingSortPorDigito(arr, exp);
+        }
+    }
+
+    // Obtener el valor máximo del arreglo
+    private static int obtenerMaximo(int[] arr) {
+        int max = arr[0];
+        for (int num : arr) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
+    }
+
 }
