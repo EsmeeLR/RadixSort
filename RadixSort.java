0a69b8a654679932e31ac48f64bcee864ba606af
EsmeeLR <esmeralda.lunaromano14mail.com>
2025-11-23 17:35:50 -0600

    Modificaciones con salida.txt

--- RadixSort.java
+++ RadixSort.java
@@ -1,61 +1,46 @@
+import java.io.*;
+
 public class RadixSort {
 
-    // Método principal
     public static void radixSort(int[] arr) {
         int max = obtenerMaximo(arr);
-
-        // Aplicamos counting sort para cada dígito
         for (int exp = 1; max / exp > 0; exp *= 10) {
             countingSortPorDigito(arr, exp);
         }
     }
 
-    // Obtenemos el valor máximo del arreglo
     private static int obtenerMaximo(int[] arr) {
         int max = arr[0];
-        for (int num : arr) {
-            if (num > max) {
-                max = num;
-            }
-        }
+        for (int num : arr) if (num > max) max = num;
         return max;
     }
-    
+
     private static void countingSortPorDigito(int[] arr, int exp) {
         int n = arr.length;
         int[] salida = new int[n];
         int[] conteo = new int[10];
 
-        // Contamos ocurrencias
-        for (int i = 0; i < n; i++) {
-            int digito = (arr[i] / exp) % 10;
-            conteo[digito]++;
-        }
-
-        // Acumulamos conteo
-        for (int i = 1; i < 10; i++) {
-            conteo[i] += conteo[i - 1];
-        }
-
-        // Construimos arreglo de salida
+        for (int i = 0; i < n; i++) conteo[(arr[i] / exp) % 10]++;
+        for (int i = 1; i < 10; i++) conteo[i] += conteo[i - 1];
         for (int i = n - 1; i >= 0; i--) {
             int digito = (arr[i] / exp) % 10;
             salida[conteo[digito] - 1] = arr[i];
             conteo[digito]--;
         }
-
-        // Copiamos al arreglo original
-        for (int i = 0; i < n; i++) {
-            arr[i] = salida[i];
-        }
+        for (int i = 0; i < n; i++) arr[i] = salida[i];
     }
 
-    // Finción para poder imprimir el arreglo
     public static void imprimirArreglo(int[] arr) {
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
+        for (int num : arr) System.out.print(num + " ");
         System.out.println();
     }
 
+    // Guardar arreglo en archivo
+    public static void guardarEnArchivo(int[] arr, String nombreArchivo) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(nombreArchivo));
+        for (int num : arr) {
+            bw.write(num + " ");
+        }
+        bw.close();
+    }
 }
--- RadixSortTesting.java
+++ RadixSortTesting.java
@@ -1,13 +1,31 @@
+import java.io.*;
+import java.util.*;
+
 public class RadixSortTesting {
-    public static void main(String[] args) {
-        int[] numeros = {170, 45, 75, 90, 802, 24, 2, 66};
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese la cantidad de números:");
+        int n = sc.nextInt();
+        int[] numeros = new int[n];
+
+        System.out.println("Ingrese los números:");
+        for (int i = 0; i < n; i++) {
+            numeros[i] = sc.nextInt();
+        }
 
         System.out.println("Lista original:");
         RadixSort.imprimirArreglo(numeros);
 
+        // Ordenar
         RadixSort.radixSort(numeros);
 
         System.out.println("Lista ordenada:");
         RadixSort.imprimirArreglo(numeros);
+
+        // Guardar salida en archivo
+        RadixSort.guardarEnArchivo(numeros, "salida.txt");
+        System.out.println("La lista ordenada se guardó en salida.txt");
+        sc.close();
     }
 }
